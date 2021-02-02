f7b8f61f734a8f593caa1a87315311948d669ee1
lohnsonok <lohnsonok@gmail.com>
2021-02-02 11:51:05 +0000

    fix:

--- src/App.js
+++ src/App.js
@@ -3,117 +3,131 @@ import './App.css';
 import Letter from './Letter/Letter.js'
 import Keyboard from './Keyboard/Keyboard.js'
 import Counter from './Counter/Counter.js'
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const alphabet = "AOBNDCEFUGHITWXYKLM"
-const allword = ["COTONOU"]
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const allquiz = [
+    {
+        "question": "Quel est la capital économique du Bénin ?",
+        "response": "COTONOU"
+    },
+    {
+        "question": "Quel est la capital adminstrative du Bénin ?",
+        "response": "PORTO"
+    },
+]
 
 class App extends Component {
-  state = {
-    letters: this.generateWords(),
-    keyboard : this.generateKeyboard(),
-    selection : [],
-    gameState : "en cours",
-  }
-
-  generateWords() {
-    const result = []//tableau vide
-    let oneWord = Math.floor(Math.random()* allword.length)// on chposie une index 
-    oneWord = allword[oneWord]//on choisie le mot à l'index prédéfinit
-    const word = oneWord.split('')//on découpe le mot
-    while (word.length>0) {
-      const letter = word.shift()// la constante letter reçoit la lettre à l'index 0 qu'on supprime du mot word et l'index des autes mots sont décrémenté
-      result.push(letter)// on met la constante letter dans le tableau result l'un a près l'autre
+    state = {
+        question: this.generateQuiz(),
+        keyboard: this.generateKeyboard(),
+        selection: [],
+        gameState: "en cours",
     }
-    return result
-  }
 
-  generateKeyboard() {
-    const result = []
-    const size = 19
-    const allLetters = alphabet.split('')
-    while (result.length < size) {
-      const letter = allLetters.shift()
-      result.push(letter)// on met les lettres de alphabet dans le tableau
+    generateQuiz() {
+        const result = {
+            letters: [],
+            question: ''
+        }//
+        let randomquiz = Math.floor(Math.random() * allquiz.length)// on chposie une index 
+
+        result.question = allquiz[randomquiz].question//on choisie le mot à l'index prédéfinit
+        let oneWord = allquiz[randomquiz].response//on choisie le mot à l'index prédéfinit
+        const word = oneWord.split('')//on découpe le mot
+        while (word.length > 0) {
+            const letter = word.shift()// la constante letter reçoit la lettre à l'index 0 qu'on supprime du mot word et l'index des autes mots sont décrémenté
+            result.letters.push(letter)// on met la constante letter dans le tableau result l'un a près l'autre
+        }
+        return result
     }
-    return result
-  }
 
-  getFeedback(letter) {
-    const { selection } = this.state
-    return selection.includes(letter)
-  }// cette fonction permet de vérifier si une lettre est inclue dans le tableau letter
-
-  handleClick = letter => {
-    const { selection, gameState } = this.state
-    if(gameState == "en cours") {
-      this.setState({selection: [...selection, letter]}, this.gameState)
+    generateKeyboard() {
+        const result = []
+        const size = 26
+        const allLetters = alphabet.split('')
+        while (result.length < size) {
+            const letter = allLetters.shift()
+            result.push(letter)// on met les lettres de alphabet dans le tableau
+        }
+        return result
     }
-  }
 
-  newGame = () => {
-    this.setState({selection: [], letters: this.generateWords(), gameState : "en cours" })
-  }
+    getFeedback(letter) {
+        const { selection } = this.state
+        return selection.includes(letter)
+    }// cette fonction permet de vérifier si une lettre est inclue dans le tableau letter
 
-  trying = () => {
-    const {letters, selection} = this.state
-    return selection.filter(elt => !letters.includes(elt)).length//return la taille des élément choisit non compris dans le tableau
-  }
-
-  gameState = () => {
-    const {letters, selection} = this.state
-    const lastTests = 10 - this.trying()
-    const findWord = letters.filter(elt => selection.includes(elt)).length === letters.length
-    if (lastTests > 0 && findWord) {
-      this.setState({gameState : "gagnée"})
-    } else if (lastTests > 0 ) {
-      return
-    } else {
-      this.setState({gameState : "perdue"})
+    handleClick = letter => {
+        const { selection, gameState } = this.state
+        if (gameState == "en cours") {
+            this.setState({ selection: [...selection, letter] }, this.gameState)
+        }
     }
-  }
 
-  render() {
-    const { letters, keyboard } = this.state
+    newGame = () => {
+        this.setState({ selection: [], question: this.generateQuiz(), gameState: "en cours" })
+    }
 
-    return (
-      <div className="hangman">
-        <div className="header">
-          <h1 className="title">Jeu du pendu</h1>
-          <Counter
-              counter = {this.trying()}
-              gameState = {this.state.gameState}
-              />
-          <button className="restart" onClick={this.newGame}>Reprendre le Jeu</button>
-        </div>
-        <p className="question">Quel est la capital économique du Bénin ? </p>
+    trying = () => {
+        const { question, selection } = this.state
+        return selection.filter(elt => !question.letters.includes(elt)).length//return la taille des élément choisit non compris dans le tableau
+    }
 
-        <div className="game">
-          <div className="content"> 
-            { letters.map((letter, index) => (
-              <Letter
-                letter={letter}
-                feedback={this.getFeedback(letter) ? "visible" : "hidden"}
-                key={index}
-              />
-            ))}
-          </div>
-        </div>
+    gameState = () => {
+        const { question, selection } = this.state
+        const lastTests = 10 - this.trying()
+        const findWord = question.letters.filter(elt => selection.includes(elt)).length === question.letters.length
+        if (lastTests > 0 && findWord) {
+            this.setState({ gameState: "gagnée" })
+        } else if (lastTests > 0) {
+            return
+        } else {
+            this.setState({ gameState: "perdue" })
+        }
+    }
 
-        <div className="keyboard">
-          { keyboard.map((letter, index) => (
-            <Keyboard
-              letter={letter}
-              key={index}
-              onClick={this.handleClick}
-              feedback={this.getFeedback(letter) ? "gray" : "#ffa500"}
-              />
-          ))}
-        </div>
-      </div>
+    render() {
+        const { question, keyboard } = this.state
 
-    )
-  }
+        return (
+            <div className="hangman">
+                <div className="header">
+                    <h1 className="title">Jeu du pendu</h1>
+                    <Counter
+                        counter={this.trying()}
+                        gameState={this.state.gameState}
+                    />
+                    <button className="restart" onClick={this.newGame}>Reprendre le Jeu</button>
+                </div>
+                <p className="question">{question.question}</p>
+
+                <div className="game">
+                    <div className="content">
+                        {question.letters.map((letter, index) => (
+                            <Letter
+                                letter={letter}
+                                feedback={this.getFeedback(letter) ? "visible" : "hidden"}
+                                key={index}
+                            />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="keyboard">
+                    {keyboard.map((letter, index) => (
+                        <Keyboard
+                            letter={letter}
+                            key={index}
+                            onClick={this.handleClick}
+                            feedback={this.getFeedback(letter) ? "gray" : "#ffa500"}
+                        />
+                    ))}
+                </div>
+            </div>
+
+        )
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './Card'
-import GuessCount from './GuessCount'
 // import './index.css';
- import App from './App';
-import {CoolComponent} from './CoolComponent'
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App  />
-    {/* <CoolComponent adjective="awesome" />
-    <CoolComponent />
-    <div className="memory">
-        <GuessCount guesses={0} />
-        <Card card="😀" feedback="hidden" />
-        <Card card="🎉" feedback="justMatched" />
-        <Card card="💖" feedback="justMismatched" />
-        <Card card="🎩" feedback="visible" />
-        <Card card="🐶" feedback="hidden" />
-        <Card card="🐱" feedback="justMatched" />
-    </div> */}
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
